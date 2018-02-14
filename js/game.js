6850843ffc0bcb6813def0b44e7cfa1279d4d054
Arena <dhiogoboza@gmail.com>
2018-02-14 00:06:17 -0300

    DOING: Choose snake avatar

--- js/game.js
+++ js/game.js
@@ -207,6 +207,9 @@ function connect(server) {
         document.getElementById('beta').style.display = "none";
         document.getElementById('ads').style.display = "none";
         document.getElementById('social-buttons').style.display = "none";
+        
+        // setup snake head
+        head_canvas["current"] = head_canvas["up"];
 
         // init ui items
         leaderBoardTable = document.getElementById("leader-board-table");
@@ -738,7 +741,6 @@ function initTiles() {
     dctx.fillRect(s35, s15, s15, s15);
 
     head_canvas["up"] = canvas;
-    head_canvas["current"] = head_canvas["up"];
     
     // head down
     canvas = document.createElement("canvas");
@@ -828,6 +830,50 @@ function drawCircle(dctx, s18, c1, c2, c3, radius) {
     }
 }
 
+function initAvatarChooser() {
+    var c = document.getElementById("avatar");
+    var items = 10;
+    
+    var s = 0.2 * item_size;
+    
+    c.height = 1.0 * item_size + 2 * s;
+    c.width = items * item_size + 2 * s;;
+    
+    var ctx = c.getContext("2d");
+    ctx.fillStyle = TILES[0].item;
+    ctx.fillRect(0, 0, c.width, c.height);
+    
+    ctx.strokeStyle = grid_color;
+    
+    ctx.beginPath();
+    ctx.moveTo(0, s);
+    ctx.lineTo(c.width, s);
+    ctx.stroke();
+    
+    ctx.beginPath();
+    ctx.moveTo(0, s + item_size);
+    ctx.lineTo(c.width, s + item_size);
+    ctx.stroke();
+    
+    x = s;
+    for (i = 0; i < items; i++) {
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, c.height);
+        ctx.stroke();
+        
+        if (i > 1 && i < 8) {
+            ctx.drawImage(TILES[TILES.length - 1].item, x, s, item_size_1, item_size_1);
+            
+            if (i == 7) {
+                ctx.drawImage(head_canvas["right"], x, s, item_size_1, item_size_1);
+            }
+        }
+        
+        x += item_size;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
     var c = document.getElementById("canvas");
 
@@ -892,6 +938,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
             connect(server);
         }
     };
+    
+    initAvatarChooser();
     
     window.addEventListener('keydown', keyPressed, false);
 });
