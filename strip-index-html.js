fd5e88b57affbfdc909ec9585b4d6ee574f38f8a
Ruben O. Chiavone <ruben.ochiavone@gmail.com>
2018-06-23 21:30:13 -0300

    Added 'index.html' build script

---
+++ strip-index-html.js
@@ -0,0 +1,30 @@
+var DOMParser = require('xmldom').DOMParser;
+var XMLSerializer = require('xmldom').XMLSerializer;
+var fs = require('fs'),
+    path = require('path'),    
+    filePath = path.join(__dirname, 'index.html');
+
+fs.readFile(filePath, 'utf8', function(error, data) {
+    if (!error) {
+        var parser = new DOMParser();
+        var document = new DOMParser().parseFromString(data, "text/html");
+        var scripts = document.getElementsByTagName("script");
+        for (var i = 0; i < scripts.length; i++) {
+            var script = scripts[i];
+            var src = script.getAttribute("src");
+            if (src.startsWith("js/")) {
+                document.removeChild(script);
+            }
+        }
+        var mainScript = document.createElement("script");
+        mainScript.setAttribute("src", "js/main.js");
+        document.getElementsByTagName("body")[0].appendChild(mainScript);
+        fs.writeFile("dist/index.html", new XMLSerializer().serializeToString(document), function(error) {
+            if (error) {
+                return console.log(error);
+            }
+        }); 
+    } else {
+        console.log(error);
+    }
+});
